de04e57a069ba810aa8e05888032c130dd92b16b
Jack Van Holland <jvanhol1@jhu.edu>
2020-09-12 12:56:52 -0400

    added the basic activity structure

---
+++ app/src/main/java/com/example/coviduniversity/FFC.java
@@ -0,0 +1,14 @@
+package com.example.coviduniversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FFC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f_f_c);
+    }
+}
