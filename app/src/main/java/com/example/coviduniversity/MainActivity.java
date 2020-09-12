5d40c7ac8162bd7f8268f2abc05fe8a93c174c96
Jack Van Holland <jvanhol1@jhu.edu>
2020-09-11 22:12:07 -0400

    beginnings of firebase

--- app/src/main/java/com/example/coviduniversity/MainActivity.java
+++ app/src/main/java/com/example/coviduniversity/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.coviduniversity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase dbase;
+    private DatabaseReference dbref;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    AlertDialog signOutWarning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        dbase = FirebaseDatabase.getInstance();
+        dbref = dbase.getReference();
+        user = auth.getCurrentUser();
+
+
     }
 }
