724ffca1f342a75be9ffed18e24773caa405d49b
rzhai2 <rzhai2@jh.edu>
2020-09-12 22:00:17 -0400

    set the portrait as default for Brody, FFC and Gym

--- app/src/main/java/com/example/coviduniversity/FFC.java
+++ app/src/main/java/com/example/coviduniversity/FFC.java
@@ -3,6 +3,7 @@ package com.example.coviduniversity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,7 @@ public class FFC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_f_f_c);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mButtonToMenu = findViewById(R.id.button_to_menu);
 
--- app/src/main/java/com/example/coviduniversity/Gym.java
+++ app/src/main/java/com/example/coviduniversity/Gym.java
@@ -2,6 +2,7 @@ package com.example.coviduniversity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Gym extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class Gym extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
