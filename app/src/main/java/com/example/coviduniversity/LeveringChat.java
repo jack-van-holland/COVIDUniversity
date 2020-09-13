9ee9e54a709d54ce7d8ae58cf0270e022cbc921c
garycloudyang <garycloudyang@gmail.com>
2020-09-13 01:27:16 -0400

    Fixed words

--- app/src/main/java/com/example/coviduniversity/LeveringChat.java
+++ app/src/main/java/com/example/coviduniversity/LeveringChat.java
@@ -39,7 +39,7 @@ public class LeveringChat extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_chat);
+        setContentView(R.layout.activity_levering_chat);
 
         chat = new ArrayList<>();
 
