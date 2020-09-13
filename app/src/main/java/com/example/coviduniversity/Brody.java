b919807d71e7a8e738bc1b5e5c664286022d471c
rzhai2 <rzhai2@jh.edu>
2020-09-12 23:04:43 -0400

    make the window scroll up when keyboard pops up

--- app/src/main/java/com/example/coviduniversity/Brody.java
+++ app/src/main/java/com/example/coviduniversity/Brody.java
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
